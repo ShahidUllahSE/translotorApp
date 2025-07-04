b22325b670754072feddb786f72329b3f7c37196
Shahid Ullah <114725262+ShahidUllahSE@users.noreply.github.com>
2025-07-04 18:35:09 +0500

    ss

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import cors from "cors"; // <-- Add this line
+
 // import messageRoutes from "./routes/message.routes";
 import translateRoutes from "./routes/translate.routes";
 import userRoutes from "./routes/user.routes";
@@ -9,12 +11,15 @@ import chatRoutes from "./routes/chat.routes";
 dotenv.config();
 
 const app = express();
+
+app.use(cors()); // <-- Enable CORS for all origins
+
 app.use(express.json());
+
+// Routes
 // app.use("/api/messages", messageRoutes);
 app.use("/api", translateRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/chat', chatRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/chat", chatRoutes);
 
 export default app;
-// 684c58cf28a966c235f6a51b
-// 684c58e528a966c235f6a51e
