f3ece644d0495cc191064753869311cbc28fc72b
Shahid Ullah <114725262+ShahidUllahSE@users.noreply.github.com>
2025-07-04 18:44:26 +0500

    ds

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import { registerUser, getUsers, getUserById } from '../controllers/user.controller';
+import { registerUser, getUsers, getUserById, loginUser } from '../controllers/user.controller';
 
 const router = express.Router();
 
 // Register a new user
 router.post('/register', registerUser);
+router.post('/login', loginUser);
 
 // Get all users
 router.get('/users', getUsers);
